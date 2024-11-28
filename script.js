62cf14287e51461464d5c23456f646cd4288c1bc
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-11-28 11:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 const tbody = document.getElementById("output");
-
+const tr = document.createElement('tr');
+tr.textContent = "Loading...";
+tbody.append(tr);
 
 
 
@@ -9,15 +11,7 @@ const promise1 = new Promise((res,rej)=> {
 	const td1 = document.createElement('td');
 	const td2 = document.createElement('td');
 
-	
-	td1.textContent = "Loading...";
-	tr.append(td1);
-	tr.setAttribute('id','loading');
-	tbody.append(tr);
-
 	setTimeout(()=> {
-	tr.textContent = "";
-	tr.removeAttribute('id');
 	td1.textContent = "Promise 1";
 	td2.textContent = 2;
 	tr.append(td1);
@@ -30,38 +24,9 @@ const promise2 = new Promise((res,rej)=> {
   const tr = document.createElement('tr');
 	const td1 = document.createElement('td');
 	const td2 = document.createElement('td');
-	
-	// td1.textContent = "Loading...";
-	tr.append(td1);
-	tr.setAttribute('id','loading');
-	tbody.append(tr);
 
 	setTimeout(()=> {
-	tr.textContent = "";
-	tr.removeAttribute('id');
 	td1.textContent = "Promise 2";
-	td2.textContent = 3;
-	tr.append(td1);
-	tr.append(td2);
-	res(tr);
-	},3000)
-})
-
-const promise3 = new Promise((res,rej)=> {
-  const tr = document.createElement('tr');
-	const td1 = document.createElement('td');
-	const td2 = document.createElement('td');
-	
-	
-	// td1.textContent = "Loading...";
-	tr.append(td1);
-	tr.setAttribute('id','loading');
-	tbody.append(tr);
-
-	setTimeout(()=> {
-	tr.textContent = "";
-	tr.removeAttribute('id');
-	td1.textContent = "Promise 3";
 	td2.textContent = 1;
 	tr.append(td1);
 	tr.append(td2);
@@ -69,18 +34,40 @@ const promise3 = new Promise((res,rej)=> {
 	},1000)
 })
 
+const promise3 = new Promise((res,rej)=> {
+  const tr = document.createElement('tr');
+	const td1 = document.createElement('td');
+	const td2 = document.createElement('td');
+
+	setTimeout(()=> {
+	td1.textContent = "Promise 3";
+	td2.textContent = 3;
+	tr.append(td1);
+	tr.append(td2);
+	res(tr);
+	},3000)
+})
+
+
+
+
+
+
+
 Promise.all([promise1,promise2,promise3]).then((val) => {
+  tr.remove();
 	val.forEach((row)=> {
 	  tbody.append(row);
 	})
-    const tr = document.createElement('tr');
+  
+  const tr1 = document.createElement('tr');
 	const td1 = document.createElement('td');
 	const td2 = document.createElement('td');
 	td1.textContent = "Total";
 	td2.textContent = 6;
 
-	tr.append(td1);
-	tr.append(td2);
-	tbody.append(tr);
+	tr1.append(td1);
+	tr1.append(td2);
+	tbody.append(tr1);
 	
 })
